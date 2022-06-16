22662cbcdeb13557a0a2b29970dd7f628b4d4871
Artiom <examplejs@gmail.com>
2022-06-16 14:53:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// "use strict";
+
+// // alert("Message!");
+
+// // const result = confirm("Are you here?");
+// // console.log(result);
+
+
+// // const answer = + prompt("Are you 18?", "18");
+// // console.log(answer + 5);
+
+// // const answers = [];
+
+// // // answers[0] = prompt("What your name?", "");
+// // // answers[1] = prompt("What your surname?", "");
+// // // answers[2] = prompt("What your age?", "");
+
+
+// // console.log(typeof(answers));
+
+
+// // const category = "toys";
+
+// // console.log(`https://someurl.com/${category}/5`);
+
+// // const userName = prompt("Name, please");
+
+// // alert(`HI, ${userName}`);
+
+// console.log(`string text line 1
+// string text line 2`);
+
+let incr = 10,
+    decr = 10;
+
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(++decr);
+
+
+
+
+
+
+
+
+
+
